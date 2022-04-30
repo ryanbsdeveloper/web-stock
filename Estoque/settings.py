ed7b44573ecf306e588442030894e501431abacc
Ryan Silva <ryanprogrammerpy@gmail.com>
2022-04-30 06:36:29 -0300

    2

--- Estoque/settings.py
+++ Estoque/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$8*$y4%k5ozcsotw87n=dd)^epx@$v9eg@8z^&hdfov=ibzo=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -216,5 +216,5 @@ CSRF_COOKIE_HTTPONLY = True
 SECURE_HSTS_PRELOAD = True
 X_FRAME_OPTIONS ='DENY'
 
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 
