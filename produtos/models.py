b97e1198f75a9eedfa8ab254eaae0aa6dafeab54
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-05-08 04:20:17 -0300

    Update model

--- produtos/models.py
+++ produtos/models.py
@@ -7,10 +7,11 @@ class EstoqueModel(models.Model):
     nome_produto = models.CharField(max_length=50, verbose_name='Nome do produto')
     imagem_produto = models.ImageField(upload_to='produtos-img/%Y')
     quantidade = models.IntegerField(verbose_name='Quantidade', default=1, blank=False)
-    validade = models.DateField(verbose_name='Validade do produto',null=True, blank=True)
+    validade = models.CharField(max_length=50, verbose_name='Validade do produto',null=True, blank=True)
     marca = models.CharField(max_length=50, verbose_name='Marca do produto')
     preco = models.FloatField(verbose_name='Preço unitário ')
     created_at = models.DateTimeField(auto_now_add=True,  null=True)
+
 
     def __str__(self) -> str:
         return self.nome_produto
