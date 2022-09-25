9252d2bbdd79dbc83b72b03e61f2284cb6f9594a
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-09-25 03:56:43 -0300

    Update setting

--- Estoque/settings.py
+++ Estoque/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$8*$y4%k5ozcsotw87n=dd)^epx@$v9eg@8z^&hdfov=ibzo=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://webstock.herokuapp.com']
 
