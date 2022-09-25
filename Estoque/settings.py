e4fff29bcbf3acf987c89e2a091f7d8d45fc9176
ryanbsdeveloper <ryanbsdeveloper@gmail.com>
2022-09-25 03:52:04 -0300

    Update ALLOWED HOSTS

--- Estoque/settings.py
+++ Estoque/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$8*$y4%k5ozcsotw87n=dd)^epx@$v9eg@8z^&hdfov=ibzo=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://webstock.herokuapp.com']
 
 # Application definition
 
